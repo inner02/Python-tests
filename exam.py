9e2c2f918a430e4058ccbfbcf771c888d2d48590
inner02 <157796160+inner02@users.noreply.github.com>
2024-03-12 16:01:34 +0000

    Create exam.py

---
+++ exam.py
@@ -0,0 +1,19 @@
+def calculate_grade(mark):
+    if mark < 1 or mark > 100:
+        return 'Error: marks must be between 1 and 100'
+    elif mark < 50:
+        return 'Fail'
+    elif mark <= 60:
+        return 'Pass'
+    elif mark <= 70:
+        return 'Merit'
+    else:
+        return 'Distinction'
+
+def main():
+    mark = int(input("Enter the exam mark for the student (between 1 and 100): "))
+    grade = calculate_grade(mark)
+    print("Exam grade:", grade)
+
+if __name__ == "__main__":
+    main()
