20443fb65aff38cee19328b634a63ea41dede739
inner02 <157796160+inner02@users.noreply.github.com>
2024-03-13 11:14:52 +0000

    Create squares.py

---
+++ squares.py
@@ -0,0 +1,15 @@
+# Squares.py
+
+# Task 1 - Squares
+
+# Start a counter at 1
+num = 1
+
+# Loop until the square of the number exceeds 2000
+while num ** 2 <= 2000:
+    # Calculate the square
+    square = num ** 2
+    # Display the number and its square
+    print(f"{num} squared is {square}")
+    # Increment the counter
+    num += 1
