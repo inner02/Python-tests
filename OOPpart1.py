a848602f4b63538dd092945a9d13b154106a0881
inner02 <157796160+inner02@users.noreply.github.com>
2024-03-14 15:42:12 +0000

    Update OOPpart1.py

--- OOPpart1.py
+++ OOPpart1.py
@@ -1,12 +1,11 @@
-# Part 1
+
 class Student:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-# Create two student objects
+
 student1 = Student("Alice", 18)
 student2 = Student("Bob", 20)
 
-# Get the age of one student
 print(student1.age)
