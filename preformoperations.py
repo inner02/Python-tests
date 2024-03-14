17b01403bda56abec42d513e8406c7420a13330b
inner02 <157796160+inner02@users.noreply.github.com>
2024-03-14 12:25:49 +0000

    Create preformoperations.py

---
+++ preformoperations.py
@@ -0,0 +1,12 @@
+def perform_operations():
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    
+    sum_result = num1 + num2
+    sub_result = num1 - num2
+    multiply_result = num1 * num2
+    
+    print(f'Sum is {sum_result}, subtraction is {sub_result}, and multiplication is {multiply_result}')
+perform_operations()
