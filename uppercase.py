1f96395af33d069c19a8cb463a03121e9dae2539
inner02 <157796160+inner02@users.noreply.github.com>
2024-03-15 09:46:30 +0000

    Create uppercase.py

---
+++ uppercase.py
@@ -0,0 +1,6 @@
+def upper_case(user_input):
+  return user_input.upper()
+
+user_input = input("Enter any word you want to capitalise:")
+result = upper_case(user_input)
+print("Capitalised word:", result)
